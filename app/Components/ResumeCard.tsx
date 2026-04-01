ac4c6c911d39353c5eea07a5cf936e74a48d88c6
savageme6ix <maingithomas803@gmail.com>
2026-04-01 10:16:19 +0300

    Displayed titles

--- app/Components/ResumeCard.tsx
+++ app/Components/ResumeCard.tsx
@@ -3,7 +3,15 @@ import {Link} from 'react-router';
 const ResumeCard = ({resume} : {resume: Resume}) => {
   return (
     <Link to={`/resume/${resume.id}`} className='resume-card animate-in fade-in duration-1000ms'>
-        ResumeCard
+        <div className='flex flex-col gap-2'>
+            <h2 className='!text-black font-bold break-words'>
+                {resume.companyName}
+            </h2>
+            <h3 className='text-lg break-words text-gray-500'>
+                {resume.jobTitle}
+            </h3>
+        </div>  
+        
     </Link>
   )
 }
