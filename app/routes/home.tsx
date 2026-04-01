b59d0f15ef36a359cfa553aad77f4ba6eb896719
savageme6ix <maingithomas803@gmail.com>
2026-04-01 07:31:14 +0300

    Implemented styling for the header and overall background

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,12 +3,17 @@ import type { Route } from "./+types/home";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Resumind" },
-    { name: "description", content: "Welcome to React Router!" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <main>
-
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track your Applications & Resume Rating</h1>
+        <h2>Review your submissions and check AI-powered feedback</h2>
+      </div>
+    </section>
   </main>;
 }
