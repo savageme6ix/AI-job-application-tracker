fa65ae0662c8e1ce4acf578822a63c3a7b0a8c96
savageme6ix <maingithomas803@gmail.com>
2026-04-02 12:14:21 +0300

    Wrote metadata for the auth component route

--- app/routes/Auth.tsx
+++ app/routes/Auth.tsx
@@ -1,4 +1,11 @@
+import type { Route } from "./+types/auth";
 
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Resumind | Auth" },
+    { name: "description", content: "Log into your account" },
+  ];
+}
 const Auth = () => {
   return (
     <div>Auth</div>
