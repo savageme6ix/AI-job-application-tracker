30e4432c122940b1d38f460d99504a61670d8192
savageme6ix <maingithomas803@gmail.com>
2026-04-02 14:21:20 +0300

    installed and imported react dropzone

--- app/Components/FileUploader.tsx
+++ app/Components/FileUploader.tsx
@@ -1,7 +1,9 @@
+import React, {useCallback} from 'react';
+import {useDropzone} from 'react-dropzone';
 
 const FileUploader = () => {
   return (
-    <div>FileUploader</div>
+    <div className="w-full gradient-border">FileUploader</div>
   )
 }
 
