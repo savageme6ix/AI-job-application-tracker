e5d182287a8d1eef717cdddd5217d3f4bbc660f3
savageme6ix <maingithomas803@gmail.com>
2026-04-03 16:03:33 +0300

    Displayed upload instructions to the user and added state to check if a file is uploaded

--- app/Components/FileUploader.tsx
+++ app/Components/FileUploader.tsx
@@ -1,8 +1,10 @@
 import { usePuterStore } from '~/lib/puter';
 import React, { useCallback, useEffect } from 'react';
 import { useDropzone } from 'react-dropzone';
+import { useState } from 'react';
 
 const FileUploader = () => {
+   const [file, setFile] = useState(false)
    const fs = usePuterStore((state) => state.fs);
     
   // 1. Move the hook logic directly into the main component
@@ -37,6 +39,22 @@ const FileUploader = () => {
           <div className='mx-auto w-16 h-16 flex items-center justify-center'>
             <img src='/icons/info.svg' alt='upload' className='size-20'/>
           </div>
+          {file ?(
+            <div>
+
+            </div>
+          ):(
+            <div>
+              <p className='text-lg text-gray-500'>
+                <span className='font-semi-bold'>
+                  Click to upload
+                </span> or drag and drop
+              </p>
+              <p className='text-lg text-gray-500'>
+                  PDF (max 20 MB)
+              </p>
+            </div>
+          )}
       </div>
     </div>
     </div>
