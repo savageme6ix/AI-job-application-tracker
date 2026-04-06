f73138ccdeae4a843e77b8e94d9f8d9b65f02d75
savageme6ix <maingithomas803@gmail.com>
2026-04-06 10:21:23 +0300

    Added metadata

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,4 +1,13 @@
-import { useParams } from "react-router"
+import { useParams } from "react-router";
+import type { Route } from "./+types/auth";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Resumind | Review" },
+    { name: "description", content: "Detailed overview of your resume" },
+  ];
+}
+
 const resume = () => {
   const { id } = useParams();
   return (
