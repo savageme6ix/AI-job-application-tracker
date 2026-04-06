22a0fbda0ed2fb6728cd8439cd1b3bcf0fc4dce6
savageme6ix <maingithomas803@gmail.com>
2026-04-06 12:06:08 +0300

    Fixed styling issues

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -53,20 +53,19 @@ const Resume = () => {
         </Link>
       </nav>
       <div className="flex flex-row w-full max-lg:flex-col-reverse">
-        <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-[100vh] sticky top-0 items-center justify-center]">
-          {imageUrl && resumeUrl && (
-            <div className=" animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-2xl:h-fit w-fit">
-              <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
-                <img
-                  src={imageUrl}
-                  className="w-full h-full object-contain rounded-2xl"
-                  title="resume"
-                />
-              </a>
-            </div>
-          )}
-
-        </section>
+      <section className="feedback-section bg-[url('/images/bg-small.svg')] bg-cover min-h-screen sticky top-0 flex items-center justify-center">
+        {imageUrl && resumeUrl && (
+        <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-2xl:h-fit w-fit">
+          <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
+            <img
+              src={imageUrl}
+              className="w-full h-full object-contain rounded-2xl"
+              title="resume"
+            />
+          </a>
+        </div>
+      )}
+  </section>
 
       </div>
     </main>
