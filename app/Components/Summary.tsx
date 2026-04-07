e7217b90e7f222ffcc449dab5057cfc7a4037132
savageme6ix <maingithomas803@gmail.com>
2026-04-07 17:55:57 +0300

    Created category component for further details on varibles used to calc score

--- app/Components/Summary.tsx
+++ app/Components/Summary.tsx
@@ -1,10 +1,23 @@
-import ScoreGauge from "./ScoreGauge"
+import ScoreGauge from "./ScoreGauge";
+
+const Category =({title,score}:{title:string, score:number})=>{
+  return(
+    <div className="resume-summary">
+      {title}
+    </div>
+  )
+}
 
 const Summary = ({feedback}:{feedback: Feedback}) => {
   return (
     <div className="bg-white rounded-2xl shadow-md w-full">
       <div className="flex flex-row items-center p-4 gap-8">
           <ScoreGauge score={feedback.overallScore}/>
+
+          <div className="flex flex-col gap-2">
+              <h2 className="text-2xl font-bold">Your Resume Score</h2>
+              <p className="text-sm text-gray-500">This score is calculated based on the variables listed below</p>
+          </div>
       </div>
     </div>
   )
