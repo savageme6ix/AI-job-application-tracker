ab84201d402d042c210a3f622bdcec1fbad0fca2
savageme6ix <maingithomas803@gmail.com>
2026-04-09 16:40:47 +0300

    Created wipe component for deletion and route

--- app/routes.ts
+++ app/routes.ts
@@ -4,4 +4,5 @@ export default [index("routes/home.tsx"),
     route('/auth', 'routes/auth.tsx'),
     route('/upload', 'routes/Upload.tsx'),
     route('/resume/:id', 'routes/Resume.tsx'),
+    route('/wipe', 'routes/wipe.tsx'),
 ] satisfies RouteConfig;
