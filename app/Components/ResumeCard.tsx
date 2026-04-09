e5dd25826f629c77fd7e0caea7b21b2be27a6fff
savageme6ix <maingithomas803@gmail.com>
2026-04-09 15:12:41 +0300

    Provided saftey check incase user doesnt input company name and job title

--- app/Components/ResumeCard.tsx
+++ app/Components/ResumeCard.tsx
@@ -7,6 +7,7 @@ import { useState } from 'react';
 const ResumeCard = ({resume} : {resume: Resume}) => {
     const {fs} = usePuterStore();
     const [resumeUrl, setResumeUrl] = useState('')
+
   useEffect(()=>{
     const loadResume: ()=> Promise<void> = async()=>{
     const blob = await fs.read(resume.imagePath);
@@ -21,12 +22,13 @@ const ResumeCard = ({resume} : {resume: Resume}) => {
     <Link to={`/resume/${resume.id}`} className='resume-card animate-in fade-in duration-1000ms'>
     <div className='resume-card-header'>
         <div className='flex flex-col gap-2'>
-            <h2 className='!text-black font-bold break-words'>
+           {resume.companyName &&  <h2 className='!text-black font-bold break-words'>
                 {resume.companyName}
-            </h2>
-            <h3 className='text-lg break-words text-gray-500'>
+            </h2>}
+          {resume.jobTitle &&  <h3 className='text-lg break-words text-gray-500'>
                 {resume.jobTitle}
-            </h3>
+            </h3> }
+            {!resume.companyName && !resume.jobTitle && <h2 className='!text-black font-bold'>Resume</h2>}
         </div> 
 
          <div className='flex-shrink-0'>
