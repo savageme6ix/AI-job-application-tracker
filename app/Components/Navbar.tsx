ec65744de8e0006b03a5febfe506a27813abf74a
savageme6ix <maingithomas803@gmail.com>
2026-04-11 11:46:30 +0300

    Added an account button

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -5,9 +5,14 @@ const Navbar = () => {
         <Link to="/">
             <p className="text-2xl font-bold text-gradient">RESUMIND</p>
         </Link>
+        <div className="flex gap-4">
         <Link to="/upload" className="primary-button w-fit">
             Upload Resume
         </Link>
+        <Link to="/auth" className="primary-button w-fit">
+            Account
+        </Link>
+        </div>
     </nav>
   )
 }
