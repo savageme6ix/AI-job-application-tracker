7f09fac699e23f02031d5fbfaec653bca730e67f
savageme6ix <maingithomas803@gmail.com>
2026-04-11 14:09:48 +0300

    Fixed styling issue on smaller devices

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,19 +1,21 @@
 import {Link} from "react-router"
 const Navbar = () => {
   return (
-    <nav className="navbar">
-        <Link to="/">
-            <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+   <nav className="navbar">
+    
+    <Link to="/">
+        <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+    </Link>
+
+    <div className="flex w-full justify-between gap-4 md:w-auto md:justify-end">
+        <Link to="/upload" className="primary-button flex-1 text-center py-2 text-sm md:flex-none md:px-6">
+            Upload
         </Link>
-        <div className="flex gap-4">
-        <Link to="/upload" className="primary-button w-fit">
-            Upload Resume
-        </Link>
-        <Link to="/auth" className="primary-button w-fit">
+        <Link to="/auth" className="primary-button flex-1 text-center py-2 text-sm md:flex-none md:px-6">
             Account
         </Link>
-        </div>
-    </nav>
+    </div>
+</nav>
   )
 }
 
